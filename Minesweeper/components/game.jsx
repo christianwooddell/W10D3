42ae2aa5c258fa74a4f68cdec37d12fecb26b048
Christian <cwooddel@u.rochester.edu>
2021-03-03 17:18:16 -0500

    everything but the tiles

--- Minesweeper/components/game.jsx
+++ Minesweeper/components/game.jsx
@@ -17,15 +17,32 @@ export default class Game extends React.Component{
     }
 
     updateGame() {
-        //come back to here after render 
+        if (flagged) {
+            tile.toggleFlag();
+        } else {
+            tile.explore();
+        }
+        this.setState({ board: this.state.board });
     }
 
     render() {
+        let modal;
+        if (this.state.board.lost() || this.state.board.won()) {
+            const text = this.state.board.won() ? "You won!" : "You lost!";
+            modal =
+                <div className='modal-screen'>
+                    <div className='modal-content'>
+                        <p>{text}</p>
+                        <button onClick={this.restartGame}>Play Again</button>
+                    </div>
+                </div>;
+        }
+
         return (
             <div>
-                <h1>Hey there :) !!</h1>
+                {modal}
                 <Board board={this.state.board} updateGame={this.updateGame} />
             </div>
         );
-    }   
+    }
 }
--- Minesweeper/components/tile.jsx
+++ Minesweeper/components/tile.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default class Tile extends React.Component{
     constructor(props){
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+      
     }
 
     render(){
         return (
-            "t"
+            "T"
         );
     }
 }
